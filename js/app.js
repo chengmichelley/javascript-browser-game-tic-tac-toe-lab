7cdbb4348173b13f9a7b2c2cbb796e232aa3909f
Michelle Cheng <cheng.michelley@gmail.com>
2026-01-06 23:51:19 -0500

    Step 5

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const board = [];
-const turn = [];
-const winner = [];
-const tie = [];
+const winningCombos = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [3,4,5],
+    [6,7,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+]; 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board;
+let turn;
+let winner;
+let tie;
 
 /*------------------------ Cached Element References ------------------------*/
 
